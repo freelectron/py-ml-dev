fb7e1dae4e704c807e778cac3fefc4e0edbffdfc
freelectron <doermax@outlook.com>
2025-06-16 10:42:56 +0200

    Refactored collab filtering

--- py_ml_dev/ir/collab_filter/main.py
+++ py_ml_dev/ir/collab_filter/main.py
@@ -1,53 +1,49 @@
 import logging
+import os
 
+import matplotlib.pyplot as plt
+import mlflow
+import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from torch.utils.data import Dataset, DataLoader
+from sklearn.preprocessing import LabelEncoder
 from torch.optim import SGD
+from torch.utils.data import DataLoader, Dataset
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s | %(message)s")
+formatter = logging.Formatter(
+    "%(asctime)s - %(filename)s - %(levelname)s | %(message)s"
+)
 handler = logging.StreamHandler()
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-ratings = pd.read_csv("../../../static/data/movielens/ml-100k/u.data", delimiter='\t', header=None, names=['user','movie','rating','timestamp'])
-movies = pd.read_csv("../../../static/data/movielens/ml-100k/u.item",  delimiter='|', encoding='latin-1',
-                     usecols=(0,1), names=('movie','title'), header=None)
-users = pd.read_csv("../../../static/data/movielens/ml-100k/u.user", delimiter="|", header=None, usecols=(0,1,2), names=("user", "age", "gender"))
-ratings_original = ratings.copy(deep=True)
-ratings = ratings.merge(movies, on='movie').merge(users, on="user")
-
-le_user = LabelEncoder()
-le_movie = LabelEncoder()
-
-ratings["userId"] = le_user.fit_transform(ratings["user"].values)
-ratings["titleId"] = le_movie.fit_transform(ratings["title"].values)
-
-df_train, df_test = train_test_split(ratings[["userId", "titleId", "rating"]], test_size=0.2, random_state=3)
-
+mlflow.set_tracking_uri(os.environ.get("MLFLOW_TRACKING_URI"))
+mlflow.set_experiment("ir.collab_filtering")
 
 class MovieLens100K(Dataset):
-    def __init__(self, X, y):
-        self.X = X
-        self.y = y
+    def __init__(self, X: np.ndarray, y: np.ndarray):
+        self.device = torch.device(
+            "mps" if torch.backends.mps.is_available() else "cpu"
+        )
+        self.X = torch.Tensor(X).to(self.device)
+        self.y = torch.Tensor(y).to(self.device)
 
     def __len__(self):
-        assert len(self.X) == len(self.y)
-        return len(self.X)
+        assert self.X.size(0) == self.y.size(0)
+        return self.y.size(0)
 
     def __getitem__(self, idx: int):
         return self.X[idx], self.y[idx]
 
-train_dataloader = DataLoader(MovieLens100K(torch.Tensor(df_train[["userId", "titleId"]].values),torch.Tensor(df_train[["rating"]].values)), batch_size=64)
-test_dataloader = DataLoader(MovieLens100K(torch.Tensor(df_test[["userId", "titleId"]].values),torch.Tensor(df_test[["rating"]].values)), batch_size=64)
 
 class BasicLearner:
-    def __init__(self, dataloader_train, data_loader_test, model, loss_func, opt_func, lr, metric):
+    def __init__(
+        self, dataloader_train, data_loader_test, model, loss_func, opt_func, lr, metric
+    ):
         self.model = model
         self.dl_train = dataloader_train
         self.dl_test = data_loader_test
@@ -57,10 +53,21 @@ class BasicLearner:
         self.metric = metric
 
     def fit(self, epochs):
+        train_losses = list()
+        test_losses = list()
+        eval_metrics = list()
         for epoch in range(epochs):
             train_loss = self.train_epoch()
+            train_losses.append(train_loss)
             test_loss, metric = self.test_epoch()
-            logger.info(f"Epoch {epoch} train loss = {train_loss} | test loss = {test_loss} ; metric = {metric} ")
+            test_losses.append(test_loss)
+            eval_metrics.append(metric)
+
+            logger.info(
+                f"Epoch {epoch} train loss = {train_loss} | test loss = {test_loss} ; metric = {metric} "
+            )
+
+        return train_losses, test_losses, eval_metrics
 
     def train_epoch(self):
         losses = list()
@@ -68,7 +75,7 @@ class BasicLearner:
         for x, y in self.dl_train:
             self.optim.zero_grad()
             loss = self.calculate_grads(x, y)
-            losses.append(loss)
+            losses.append(loss.detach().cpu())
             self.optim.step()
 
         return torch.stack(losses).mean()
@@ -81,9 +88,9 @@ class BasicLearner:
             for x, y in self.dl_test:
                 preds = self.model(x)
                 loss = self.loss_func(preds, y)
-                losses.append(loss)
+                losses.append(loss.detach().cpu())
                 metric = self.calculate_eval_metric(preds, y)
-                val_metrics.append(metric)
+                val_metrics.append(metric.detach().cpu())
 
         return torch.stack(losses).mean(), torch.stack(val_metrics).mean()
 
@@ -101,30 +108,113 @@ class BasicLearner:
 
 
 class Perceptron(nn.Module):
-    def __init__(self, num_users, dim_user_factors, num_movies, dim_movie_factors, y_range):
+    def __init__(
+        self, num_users, dim_user_factors, num_movies, dim_movie_factors, y_range
+    ):
         super().__init__()
+        self.device = torch.device(
+            "mps" if torch.backends.mps.is_available() else "cpu"
+        )
         self.user_factors = nn.Embedding(num_users, dim_user_factors)
         self.user_bias = nn.Embedding(num_users, 1)
         self.movie_factors = nn.Embedding(num_movies, dim_movie_factors)
         self.movie_bias = nn.Embedding(num_movies, 1)
         self.y_range = y_range
         self.activation = nn.Sigmoid()
-        # TODO: add initialisation from normal distribution for all embedding layers
+        for emb in [
+            self.user_factors,
+            self.user_bias,
+            self.movie_bias,
+            self.movie_factors,
+        ]:
+            nn.init.normal_(emb.weight, 0, 0.01)
+        self.to(self.device)
 
     def forward(self, x):
-        user_embeddings = self.user_factors(x[:,0].to(torch.int64))    # batch_size x n_user_factors
-        movie_embeddings = self.movie_factors(x[:,1].to(torch.int64))  # batch_size x n_movie_factors
-        user_bias_embedding = self.user_bias(x[:,0].to(torch.int64))
+        user_embeddings = self.user_factors(
+            x[:, 0].to(torch.int64)
+        )  # batch_size x n_user_factors
+        movie_embeddings = self.movie_factors(
+            x[:, 1].to(torch.int64)
+        )  # batch_size x n_movie_factors
+        user_bias_embedding = self.user_bias(x[:, 0].to(torch.int64))
         movie_bias_embedding = self.movie_bias(x[:, 1].to(torch.int64))
-        res = (user_embeddings * movie_embeddings).sum(dim=1) + user_bias_embedding.reshape(-1) + movie_bias_embedding.reshape(-1)
+        res = (
+            (user_embeddings * movie_embeddings).sum(dim=1)
+            + user_bias_embedding.reshape(-1)
+            + movie_bias_embedding.reshape(-1)
+        )
 
-        return self.activation(res) * (self.y_range[1] - self.y_range[0]) + self.y_range[0]
+        return (
+            self.activation(res) * (self.y_range[1] - self.y_range[0]) + self.y_range[0]
+        )
 
-def mse(preds, targets):
-    return ((targets.flatten() - preds.flatten())**2).mean()
 
-n_factors = 5
+def plot_loss(losses: list, stage_labels: list):
+    fig, ax = plt.subplots(1, 1)
+    for loss_list, label in zip(losses, stage_labels):
+        ax.plot(list(range(len(loss_list))), loss_list, label=label)
+        ax.legend()
 
-model = Perceptron(len(le_user.classes_), n_factors,  len(le_movie.classes_), n_factors, (0, 5.5))
-learn = BasicLearner(train_dataloader, test_dataloader, model=model, loss_func=mse, opt_func=SGD, lr=0.05, metric=mse)
-learn.fit(10)
+    plt.show()
+
+
+if __name__ == "__main__":
+    ratings = pd.read_csv(
+        "../../../static/data/movielens/ml-100k/u.data",
+        delimiter="\t",
+        header=None,
+        names=["user", "movie", "rating", "timestamp"],
+    )
+    movies = pd.read_csv(
+        "../../../static/data/movielens/ml-100k/u.item",
+        delimiter="|",
+        encoding="latin-1",
+        usecols=(0, 1),
+        names=("movie", "title"),
+        header=None,
+    )
+    users = pd.read_csv(
+        "../../../static/data/movielens/ml-100k/u.user",
+        delimiter="|",
+        header=None,
+        usecols=(0, 1, 2),
+        names=("user", "age", "gender"),
+    )
+    ratings_original = ratings.copy(deep=True)
+    ratings = ratings.merge(movies, on="movie").merge(users, on="user")
+
+    le_user = LabelEncoder()
+    le_movie = LabelEncoder()
+
+    ratings["userId"] = le_user.fit_transform(ratings["user"].values)
+    ratings["titleId"] = le_movie.fit_transform(ratings["title"].values)
+
+    df_train, df_test = train_test_split(
+        ratings[["userId", "titleId", "rating"]], test_size=0.2, random_state=3
+    )
+
+    X_train = df_train[["userId", "titleId"]].values
+    y_train = df_train[["rating"]].values
+    X_test = df_test[["userId", "titleId"]].values
+    y_test = df_test[["rating"]].values
+    train_dataloader = DataLoader(MovieLens100K(X_train, y_train), batch_size=64)
+    test_dataloader = DataLoader(MovieLens100K(X_test, y_test), batch_size=64)
+
+    with mlflow.start_run(name=):
+        n_factors = 5
+        mse = torch.nn.MSELoss()
+        model = Perceptron(
+            len(le_user.classes_), n_factors, len(le_movie.classes_), n_factors, (0, 5.5)
+        )
+        learn = BasicLearner(
+            train_dataloader,
+            test_dataloader,
+            model=model,
+            loss_func=mse,
+            opt_func=SGD,
+            lr=0.05,
+            metric=mse,
+        )
+        train_losses, test_losses, eval_metrics = learn.fit(2)
+        plot_loss([train_losses, test_losses], ["train", "test"])
--- py_ml_dev/nlp/text_classification.py
+++ py_ml_dev/nlp/text_classification.py
@@ -2,20 +2,21 @@ import os
 import pickle
 import subprocess
 from datetime import datetime
-from typing import List, Any, Tuple, Dict
+from typing import Any, Dict, List, Tuple
 
-import pandas as pd
-import spacy
-import ray
-import mlflow
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
-from sklearn.decomposition import PCA
-from sklearn.datasets import fetch_20newsgroups
-from sklearn.linear_model import LogisticRegressionCV
-from xgboost import XGBClassifier
 import matplotlib.pyplot as plt
+import mlflow
+import pandas as pd
+import ray
+import spacy
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.decomposition import PCA
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import (ConfusionMatrixDisplay, accuracy_score,
+                             confusion_matrix)
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from xgboost import XGBClassifier
 
 
 def clean_text(text: str) -> List[str]:
@@ -34,12 +35,14 @@ def clean_text(text: str) -> List[str]:
 def clean_corpus(corpus: List[str], use_ray: bool = False) -> List[List[str]]:
     """
     Clean a list of text documents. Optionally parallelize with Ray.
-    # """
+    #"""
     if use_ray:
         ray.init(ignore_reinit_error=True, num_cpus=os.cpu_count() - 1)
+
         @ray.remote
         def remote_clean(text: str) -> List[str]:
             return clean_text(text)
+
         cleaned = ray.get([remote_clean.remote(text) for text in corpus])
         ray.shutdown()
         return cleaned
@@ -60,7 +63,7 @@ def load_pickle(path: str) -> Any:
 
 
 def vectorize_corpus(
-        cleaned_corpus: List[List[str]], vocabulary: set = None
+    cleaned_corpus: List[List[str]], vocabulary: set = None
 ) -> Tuple[CountVectorizer, Any]:
     """
     Fit a CountVectorizer on the cleaned corpus and return the vectorizer and counts.
@@ -86,7 +89,7 @@ def train_logistic_regression(X, y) -> LogisticRegressionCV:
 
 
 def train_xgboost_with_search(
-        X, y, param_dist: Dict[str, List[Any]], n_iter: int = 10
+    X, y, param_dist: Dict[str, List[Any]], n_iter: int = 10
 ) -> RandomizedSearchCV:
     """
     Train XGBoost with RandomizedSearchCV.
@@ -97,9 +100,7 @@ def train_xgboost_with_search(
     return search
 
 
-def evaluate_model(
-        model, X, y, label: str = ""
-) -> float:
+def evaluate_model(model, X, y, label: str = "") -> float:
     """Evaluate a model and print accuracy."""
     preds = model.predict(X)
     acc = accuracy_score(y, preds)
@@ -107,13 +108,14 @@ def evaluate_model(
     print(f"Accuracy for {label or model.__class__.__name__}: {acc:.4f}")
     cm = confusion_matrix(y, preds)
     displ = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
-    fig, ax = plt.subplots(1,1, figsize=(12, 12))
+    fig, ax = plt.subplots(1, 1, figsize=(12, 12))
     ax.set_title(f"Accuracy for {label or model.__class__.__name__}: {acc:.4f}")
     displ.plot(ax=ax)
     mlflow.log_figure(fig, f"confusion_matrix.png")
     plt.show()
 
     return acc
+
 
 def plot_class_frequencies(y: List[int]) -> None:
     """Plot class frequencies in the dataset."""
@@ -125,9 +127,12 @@ def plot_class_frequencies(y: List[int]) -> None:
     plt.title("Class Frequencies")
     plt.show()
 
+
 def main(name="simple_text_classification") -> None:
     # Load dataset
-    newsgroups = fetch_20newsgroups(subset="all", remove=("headers", "footers", "quotes"))
+    newsgroups = fetch_20newsgroups(
+        subset="all", remove=("headers", "footers", "quotes")
+    )
     X_train, X_test, y_train, y_test = train_test_split(
         newsgroups.data, newsgroups.target, test_size=0.2, random_state=42
     )
@@ -141,7 +146,9 @@ def main(name="simple_text_classification") -> None:
     else:
         start_time = datetime.now()
         X_train_cleaned = clean_corpus(X_train, use_ray=True)
-        print(f"Cleaning training data took {(datetime.now() - start_time).seconds} seconds.")
+        print(
+            f"Cleaning training data took {(datetime.now() - start_time).seconds} seconds."
+        )
         save_pickle(X_train_cleaned, path_X_train_cleaned)
         print(f"Saved cleaned training data to {path_X_train_cleaned}")
 
@@ -152,25 +159,37 @@ def main(name="simple_text_classification") -> None:
     n_components = 7000
     pca_estimator = PCA(n_components=n_components).fit(X_train_tfidf.toarray())
     X_train_tfidf = pca_estimator.transform(X_train_tfidf.toarray())
-    print(f"Reduced training data to {n_components} dimensions using PCA. "
-          f"Explained variance ratio: {pca_estimator.explained_variance_ratio_.sum():.4f}")
+    print(
+        f"Reduced training data to {n_components} dimensions using PCA. "
+        f"Explained variance ratio: {pca_estimator.explained_variance_ratio_.sum():.4f}"
+    )
 
     # Preprocess test data
     X_test_cleaned = clean_corpus(X_test, use_ray=True)
-    X_test_counts = vectorizer.transform([" ".join(tokens) for tokens in X_test_cleaned])
+    X_test_counts = vectorizer.transform(
+        [" ".join(tokens) for tokens in X_test_cleaned]
+    )
     X_test_tfidf = tfidf_transform(X_test_counts)
     X_test_tfidf = pca_estimator.transform(X_test_tfidf.toarray())
 
-    with mlflow.start_run(run_name=name+"-log-reg", log_system_metrics=True):
+    with mlflow.start_run(run_name=name + "-log-reg", log_system_metrics=True):
         # Train Logistic Regression
         mlflow.log_param("n_components", n_components)
         start_time = datetime.now()
         clf_logistic = train_logistic_regression(X_train_tfidf, y_train)
-        print("Training Logistic Regression with CV took", (datetime.now() - start_time).seconds, "seconds")
-        evaluate_model(clf_logistic, X_train_tfidf, y_train, "LogisticRegressionCV (train)")
-        evaluate_model(clf_logistic, X_test_tfidf, y_test, "LogisticRegressionCV (test)")
+        print(
+            "Training Logistic Regression with CV took",
+            (datetime.now() - start_time).seconds,
+            "seconds",
+        )
+        evaluate_model(
+            clf_logistic, X_train_tfidf, y_train, "LogisticRegressionCV (train)"
+        )
+        evaluate_model(
+            clf_logistic, X_test_tfidf, y_test, "LogisticRegressionCV (test)"
+        )
 
-    with mlflow.start_run(run_name=name+"-xgboost", log_system_metrics=True):
+    with mlflow.start_run(run_name=name + "-xgboost", log_system_metrics=True):
         mlflow.log_param("n_components", n_components)
         # Train XGBoost with RandomizedSearchCV
         start_time = datetime.now()
@@ -181,9 +200,17 @@ def main(name="simple_text_classification") -> None:
             "gamma": [4, 6, 8],
         }
         xgb_search = train_xgboost_with_search(X_train_tfidf, y_train, param_dist)
-        print("Training XGBoost with RandomizedSearchCV took", (datetime.now() - start_time).seconds, "seconds")
-        evaluate_model(xgb_search.best_estimator_, X_train_tfidf, y_train, "XGBoostSearch (train)")
-        evaluate_model(xgb_search.best_estimator_, X_test_tfidf, y_test, "XGBClassifier (test)")
+        print(
+            "Training XGBoost with RandomizedSearchCV took",
+            (datetime.now() - start_time).seconds,
+            "seconds",
+        )
+        evaluate_model(
+            xgb_search.best_estimator_, X_train_tfidf, y_train, "XGBoostSearch (train)"
+        )
+        evaluate_model(
+            xgb_search.best_estimator_, X_test_tfidf, y_test, "XGBClassifier (test)"
+        )
 
 
 if __name__ == "__main__":
@@ -191,7 +218,9 @@ if __name__ == "__main__":
 
     mlflow.autolog()
     mlflow.set_experiment("sklearn_simple_text_classification")
-    git_commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
-    name=f"{git_commit_hash[:7]}"
+    git_commit_hash = (
+        subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
+    )
+    name = f"{git_commit_hash[:7]}"
 
     main(name=name)
